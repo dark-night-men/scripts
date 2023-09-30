fe41c301cad1e246734183b8966f462c277e2c03
dark-night-men <visible.ocean@gmail.com>
2023-10-01 01:48:18 +0300

    Set title and author

--- books/mobp.sh
+++ books/mobp.sh
@@ -5,15 +5,22 @@ if ! [[ -f "$1" ]] ; then
     exit 1
 fi
 
+if [[ -z $2 ]]; then
+    echo Second arg must be provided, as title.
+    exit 2
+fi
+
+if [[ -z $3 ]]; then
+    echo Third arg must be provided, as author\'s name.
+    exit 3
+fi
+
 echo "arg 1 $1"
-echo "note arg 2 (if exist) means title name. default title - filename"
 
 #/usr/bin/ebook-convert "$1" "$1_clm.mobi" --pretty-print --enable-heuristics --filter-css font-family,color,margin-left,margin-right
 #/usr/bin/ebook-convert "$1" "$1_clm.mobi" --pretty-print --enable-heuristics
 
-# set -x
 
 env LC_ALL=en_US.utf8 time --format='%E' \
-/usr/bin/ebook-convert "$1" "$1_clp.mobi" --mobi-file-type new --verbose --title "${2:-$1}" --embed-font-family  'Monofur NF'
+    /usr/bin/ebook-convert "$1" "$1_clm.mobi" --mobi-file-type new --verbose --title="$2" --authors="$3" --embed-font-family  'Monofur NF'
 
-# set +x
