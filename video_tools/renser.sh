2bc510698c3202bf4e2da92b6b0903db34fd2a84
dark-night-men <visible.ocean@gmail.com>
2023-12-12 03:41:30 +0300

    rename srt and ac3 name from avi

--- video_tools/renser.sh
+++ video_tools/renser.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
-PREFIX=Rick.and.Morty.S07E01.
+PREFIX=Rick.and.Morty.S07E
 
-# set -x
+for x in {01..8}; do
+    # set -x
+
+    PREFIX_1=$PREFIX$x.
 
 
-NAME=$(ls -1 $PREFIX*.avi | sed -re "s|^$PREFIX(.*)\.avi$|\1|")
-# printf 'NAME %s \n\n' $NAME
+    NAME=$(ls -1 $PREFIX_1*.avi | sed -re "s|^($PREFIX_1.*)\.avi$|\1|")
+    # printf 'NAME %s \n\n' $NAME
 
-AC3NAME=$(ls -1 $PREFIX*1.ac3 | sed -re "s|^$PREFIX(.*)\.1\.ac3$|\1|")
+    AC3NAME=$(ls -1 $PREFIX_1*1.ac3 | sed -re "s|^($PREFIX_1.*)\.1\.ac3$|\1|")
 
-# rename -n -v "s/$AC3NAME/$NAME/" *.ac3 *.srt
-rename -v "s/$AC3NAME/$NAME/" *.ac3 *.srt
+    # rename -n -v "s/$AC3NAME/$NAME/" *.ac3 *.srt
+    rename -v "s/$AC3NAME/$NAME/" *.ac3 *.srt
 
 
 # set +x
+
+done
