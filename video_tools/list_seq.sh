979438f5016aa7eb699135384f1f81c2d58fb96c
dark-night-men <visible.ocean@gmail.com>
2025-02-20 22:04:55 +0300

    Fix initialization error, removing garbage from previous run.

--- video_tools/list_seq.sh
+++ video_tools/list_seq.sh
@@ -10,7 +10,12 @@ for disk in d e f; do
     env LC_ALL=en_US.utf8 time --format='%E' \
     ~/scripts/video_tools/list_huge_new.sh /mnt/$disk/\!${disk^^}_VIDEO  > ~/tmp/${disk}_huge.log
 
-    cp -v /tmp/skipped_files.txt /tmp/${disk}_skipped_files.txt
+    SKIPPED_FILES_FOR_DISK=/tmp/${disk}_skipped_files.txt
+    [ -f $SKIPPED_FILES_FOR_DISK ] && rm -v "$SKIPPED_FILES_FOR_DISK"
+
+    COMMON_SKIPPED_FILES=/tmp/skipped_files.txt
+    [ -f $COMMON_SKIPPED_FILES ] && mv -v "$COMMON_SKIPPED_FILES"  "$SKIPPED_FILES_FOR_DISK"
+
     sort -k1,1 -h -r ~/tmp/${disk}_huge.log > ~/tmp/${disk}_1huge.log 
 
 done
