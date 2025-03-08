fb7d3d0cb5b4bd7449ef76343eaf9567cd708bfb
dark-night-men <visible.ocean@gmail.com>
2025-03-08 21:09:55 +0300

    Get lists per disk

--- video_tools/list_seq.sh
+++ video_tools/list_seq.sh
@@ -5,6 +5,15 @@ if ((BASH_VERSINFO[0] < 4)); then
     exit 1;
 fi
 
+ALL_SKIPPED_FILES=/tmp/all_skipped_files.txt
+[ -f $ALL_SKIPPED_FILES ] && rm -v "$ALL_SKIPPED_FILES"
+
+ALL_HUGE_FILES=/tmp/all_huge_files.txt
+[ -f $ALL_HUGE_FILES ] && rm -v "$ALL_HUGE_FILES"
+
+ALL_1HUGE_FILES=/tmp/all_1huge_files.txt
+[ -f $ALL_1HUGE_FILES ] && rm -v "$ALL_1HUGE_FILES"
+
 for disk in d e f; do
 
     env LC_ALL=en_US.utf8 time --format='%E' \
@@ -14,10 +23,17 @@ for disk in d e f; do
     [ -f $SKIPPED_FILES_FOR_DISK ] && rm -v "$SKIPPED_FILES_FOR_DISK"
 
     COMMON_SKIPPED_FILES=/tmp/skipped_files.txt
-    [ -f $COMMON_SKIPPED_FILES ] && mv -v "$COMMON_SKIPPED_FILES"  "$SKIPPED_FILES_FOR_DISK"
+    [ -f $COMMON_SKIPPED_FILES ] && mv -v "$COMMON_SKIPPED_FILES"  "$SKIPPED_FILES_FOR_DISK" && cat "$SKIPPED_FILES_FOR_DISK" >> "$ALL_SKIPPED_FILES"
+
 
     sort -k1,1 -h -r ~/tmp/${disk}_huge.log > ~/tmp/${disk}_1huge.log 
 
+    cat ~/tmp/${disk}_huge.log >> "$ALL_HUGE_FILES"
+    cat ~/tmp/${disk}_1huge.log >> "$ALL_1HUGE_FILES"
+
 done
 
-vim ~/tmp/*huge.log /tmp/*skipped_files.txt 
+[ -f $ALL_HUGE_FILES ] && sort -k2,2 -h -r -o "${ALL_HUGE_FILES}"{,}
+[ -f $ALL_1HUGE_FILES ] && sort -k1,1 -h -r -o "${ALL_1HUGE_FILES}"{,}
+
+vim "$ALL_HUGE_FILES" "$ALL_1HUGE_FILES" ~/tmp/*huge.log /tmp/*skipped_files.txt 
