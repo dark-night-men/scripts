1170ce5fb153f4060e1cd9d04fbda2e9ef0c1ce8
dark-night-men <visible.ocean@gmail.com>
2025-04-02 20:44:20 +0300

    1.Extensions for c++ source files added. 2.Example for starting locations file.

--- scope.sh
+++ scope.sh
@@ -4,6 +4,11 @@
 gtags
 gtags --objdir
 
+# Example for SCOPE_STARTING_POINTS_FILE
+# /usr/lib/qt-6.8.1/include
+# /usr/lib/qt-6.8.1/mkspecs/linux-g++
+# /usr/include/opencv4
+
 SCOPE_STARTING_POINTS_FILE=scope_starting_points.txt
 SCOPE_STARTING_POINTS_FILE_ZERO=scope_starting_points.tmp.txt
 
@@ -17,10 +22,10 @@ tr '\n' '\0' < $SCOPE_STARTING_POINTS_FILE > $SCOPE_STARTING_POINTS_FILE_ZERO
 
 # ctags and cscope needs input files with different formats in case if spaces are in file/path names
 find -files0-from ./"$SCOPE_STARTING_POINTS_FILE_ZERO" \
-         -type f '(' -name "*.sh" -o -name "*.json" -o -name "*.css" -o -name "*.html" -o -name "*.ts" -o -name "*.js" -o -name "*.java" -o -name "*.vala" -o -name 'main.cpp' -o -name "*.kt" -o -name "*.kts" -o -name "*.go" -o -name "*.lua" -o -name "*.[ch]" -or -name "*.[ch]pp" -or -name "*.[ch]xx" -or -name "*.cc" -or -name "*.hh" -or -name "*.il" -or -name "*.pl" -or -name "*.py" -or -name "[mM]akefile" ')' -and -not '(' -path "*/build/*" -o -path "*/.moc/*" ')' -fprintf ctags.files.tmp '%p\n'
+         -type f '(' -name "*.c++" -o -name "*.h++" -o -name "*.sh" -o -name "*.json" -o -name "*.css" -o -name "*.html" -o -name "*.ts" -o -name "*.js" -o -name "*.java" -o -name "*.vala" -o -name 'main.cpp' -o -name "*.kt" -o -name "*.kts" -o -name "*.go" -o -name "*.lua" -o -name "*.[ch]" -or -name "*.[ch]pp" -or -name "*.[ch]xx" -or -name "*.cc" -or -name "*.hh" -or -name "*.il" -or -name "*.pl" -or -name "*.py" -or -name "[mM]akefile" ')' -and -not '(' -path "*/build/*" -o -path "*/.moc/*" ')' -fprintf ctags.files.tmp '%p\n'
 
 find -files0-from ./"$SCOPE_STARTING_POINTS_FILE_ZERO" \
--type f '(' -name "*.sh" -o -name "*.json" -o -name "*.css" -o -name "*.html" -o -name "*.ts" -o -name "*.js" -o -name "*.java" -o -name "*.vala" -o -name 'main.cpp' -o -name "*.kt" -o -name "*.kts" -o -name "*.go" -o -name "*.lua" -o -name "*.[ch]" -or -name "*.[ch]pp" -or -name "*.[ch]xx" -or -name "*.cc" -or -name "*.hh" -or -name "*.il" -or -name "*.pl" -or -name "*.py" -or -name "[mM]akefile" ')' -and -not '(' -path "*/build/*" -o -path "*/.moc/*" ')' -fprintf cscope.files.tmp '"%p"\n'
+-type f '(' -name "*.c++" -o -name "*.h++" -o -name "*.sh" -o -name "*.json" -o -name "*.css" -o -name "*.html" -o -name "*.ts" -o -name "*.js" -o -name "*.java" -o -name "*.vala" -o -name 'main.cpp' -o -name "*.kt" -o -name "*.kts" -o -name "*.go" -o -name "*.lua" -o -name "*.[ch]" -or -name "*.[ch]pp" -or -name "*.[ch]xx" -or -name "*.cc" -or -name "*.hh" -or -name "*.il" -or -name "*.pl" -or -name "*.py" -or -name "[mM]akefile" ')' -and -not '(' -path "*/build/*" -o -path "*/.moc/*" ')' -fprintf cscope.files.tmp '"%p"\n'
 
 if ! cmp ctags.files ctags.files.tmp ; then
     mv ctags.files.tmp ctags.files
