1b5db0fd3589650621fd8280fa1c928dc01b3475
dark-night-men <visible.ocean@gmail.com>
2025-04-02 21:28:36 +0300

    Convert to epub, with font IBM Plex Sans and justification left.

--- books/mobrp.sh
+++ books/mobrp.sh
@@ -17,9 +17,6 @@ fi
 
 echo "arg 1 $1"
 
-#/usr/bin/ebook-convert "$1" "$1_clm.mobi" --pretty-print --enable-heuristics --filter-css font-family,color,margin-left,margin-right
-
-
 env LC_ALL=en_US.utf8 time --format='%E' \
     /usr/bin/ebook-convert "$1" "$1_clmrp.mobi" --mobi-file-type new --verbose --title="$2" --authors="$3"\
     --embed-font-family  'Roboto Mono' --change-justification left --pretty-print
