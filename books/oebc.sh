a91ecf05e6928d4a0fb0ec6f6f6a59ba01495ec8
dark-night-men <visible.ocean@gmail.com>
2025-12-03 22:05:35 +0300

    cmd as array; font Comfortaa; justification left

--- books/oebc.sh
+++ books/oebc.sh
@@ -26,7 +26,34 @@ fi
 echo "arg 1 $1"
 echo "arg 2 $2"
 
-/usr/bin/ebook-convert "$1" "$2" --pretty-print --enable-heuristics --embed-font-family  'Comfortaa'
+# /usr/bin/ebook-convert "$1" "$2" --pretty-print --enable-heuristics --embed-font-family  'Comfortaa'
+
+cmd=(
+/usr/bin/ebook-convert "$1" "$2"
+    --pretty-print
+    --enable-heuristics
+    --verbose
+    --change-justification left
+    --title="$2" --authors="$3"
+    --embed-font-family  'Comfortaa'
+    # --extra-css "body { line-height: $LINE_HEIGHT !important; }"
+    # --extra-css ${4-""}
+    # --minimum-line-height ${5-555} 
+    # --minimum-line-height ${5-70} 
+    # --minimum-line-height 70 
+    # --minimum-line-height ${5-22} 
+    # --minimum-line-height 0
+    # --line-height 1
+    # --filter-css 'line-height'
+    # --filter-css 'font-family,line-height'
+)
+
+echo "Executing: "
+printf "%q " "${cmd[@]}"
+echo
+
+"${cmd[@]}" 
+
 
 #set -x
 #/usr/bin/ebook-convert "$1" "$2" --pretty-print --enable-heuristics --embed-font-family  'Monofur NF' --epub-flatten --verbose -d debug
