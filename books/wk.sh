b693ad03330a8207eedd24d51c72ccfdc16c2a4e
dark-night-men <visible.ocean@gmail.com>
2025-12-03 22:06:31 +0300

    cmd as array; font Comfortaa; justification left

--- books/wk.sh
+++ books/wk.sh
@@ -5,13 +5,47 @@ if ! [[ -f "$1" ]] ; then
     exit 1
 fi
 
+if [[ -z $2 ]]; then
+    echo Second arg must be provided, as title.
+    exit 2
+fi
+
+if [[ -z $3 ]]; then
+    echo Third arg must be provided, as author\'s name.
+    exit 3
+fi
+
 echo "arg 1 $1"
-echo "note arg 2 (if exist) means verbosity level 0 - by default '-v -v' - usable"
+
+cmd=(
+/usr/bin/ebook-convert "$1" "$1.epub" 
+    --pretty-print
+    --enable-heuristics
+    --verbose
+    --change-justification left
+    --title="$2" --authors="$3"
+    --embed-font-family  'Comfortaa'
+    # --extra-css "body { line-height: $LINE_HEIGHT !important; }"
+    # --extra-css ${4-""}
+    # --minimum-line-height ${5-555} 
+    # --minimum-line-height ${5-70} 
+    --minimum-line-height 70 
+    # --minimum-line-height ${5-22} 
+    # --minimum-line-height 0
+    # --line-height 1
+    # --filter-css 'line-height'
+    # --filter-css 'font-family,line-height'
+)
+
+echo "Executing: "
+printf "%q " "${cmd[@]}"
+echo
+
+"${cmd[@]}" 
 
 #/usr/local/bin/wkhtmltopdf "$1" "$1.pdf"
 
 #regular flow
-/usr/bin/ebook-convert "$1" "$1.epub" ${2:-} --pretty-print --enable-heuristics --embed-font-family  'Comfortaa'
 
 #/usr/bin/ebook-convert "$1" .epub --pretty-print --enable-heuristics --embed-font-family  'Monofur NF' --no-default-epub-cover --epub-flatten --verbose -d debug
 
