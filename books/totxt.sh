33d2a78e6f48fdb6fba124c58f35b6f55176a57f
dark-night-men <visible.ocean@gmail.com>
2026-07-04 22:05:51 +0300

    Parameter language set with ru

--- books/totxt.sh
+++ books/totxt.sh
@@ -7,4 +7,4 @@ fi
 
 echo "arg 1 $1"
 
-/usr/bin/ebook-convert "$1" "$1.txt" --pretty-print --enable-heuristics --change-justification left
+/usr/bin/ebook-convert "$1" "$1.txt" --pretty-print --enable-heuristics --change-justification left --language ru
